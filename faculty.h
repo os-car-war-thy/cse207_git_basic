bb0d84399f742fd723e460c4afffd319ed343243
os-car-war-thy <kmfahim2002@gmail.com>
2025-07-24 18:03:09 +0600

    adding faculty api

--- faculty.h
+++ faculty.h
@@ -2,4 +2,16 @@
 #define FACULTY_H
 
 
+enum designation{
+    LECUTERER,
+    ASSISTANT PROFESSOR,
+    PROFESSOR,
+
+}
+
+typedef struct faculty{
+    char *name
+}
+
+void add_faculty(){}
 #endif
